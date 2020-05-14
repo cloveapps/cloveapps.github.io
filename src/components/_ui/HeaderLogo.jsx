f9af716512715621b2158cfe090b66662bcf088e
Siva Ragavan <siva@clovehq.com>
2020-05-14 14:34:01 -0400

    Updated Logo

--- src/components/_ui/HeaderLogo.jsx
+++ src/components/_ui/HeaderLogo.jsx
@@ -7,29 +7,12 @@ const HeaderLogo = () => (
     viewBox="0 0 145 30"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g
-      id="clove-banner-30w"
-      stroke="none"
-      stroke-width="1"
-      fill="none"
-      fill-rule="evenodd"
-    >
-      <text
-        id="clove"
-        font-family="Inter-ExtraBold, Inter"
-        font-size="34"
-        font-weight="600"
-        fill="#3F3F3F"
-      >
-        <tspan x="-1" y="29">
-          clove
-        </tspan>
+    <g id="clove-banner-30w" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <text id="clove" font-family="Inter-Bold, Inter" font-size="34" font-weight="bold" fill="#3F3F3F">
+        <tspan x="-1" y="29">clove</tspan>
       </text>
-      <polygon
-        id="Polygon"
-        fill="#FF3F3F"
-        points="99 18 99 10 111 10 111 18 115 18 115 22 107 22 107 30 103 30 103 22 95 22 95 18"
-      ></polygon>
+      <polygon id="Polygon" fill="#FF3F3F"
+               points="96 18 96 10 108 10 108 18 112 18 112 22 104 22 104 30 100 30 100 22 92 22 92 18"></polygon>
     </g>
   </svg>
 )
